c3856da35ee7e2666263ed9bd42296ca5b08a0a2
Akalanka <akalankaperera128@gmail.com>
2023-04-19 19:29:54 +0530

    Added simple language toggler

--- lib/screens/home/mobile.dart
+++ lib/screens/home/mobile.dart
@@ -1,8 +1,9 @@
-import 'package:catfacts/components/common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:catfacts/components/common/common.dart';
 import 'package:catfacts/state/data/fact/fact_bloc.dart';
+import 'package:catfacts/state/ui/global/global_bloc.dart';
 
 class Mobile extends StatefulWidget {
   const Mobile({super.key});
@@ -24,6 +25,36 @@ class _MobileState extends State<Mobile> {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.facts),
+      ),
+      endDrawer: Drawer(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            GestureDetector(
+              onTap: () {
+                context.read<GlobalBloc>().add(SetLocale(context.read<GlobalBloc>().state.locale == "en" ? "ar" : "en"));
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 6, offset: const Offset(0, 4)),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    "${AppLocalizations.of(context)!.switchLanguage} (${context.read<GlobalBloc>().state.locale})",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: BlocBuilder<FactBloc, FactState>(
